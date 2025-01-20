0342b978da3a6131adfd51730d10078d33848b7a
jmrangelSTEMDO <jmrangel@stemdo.io>
2025-01-20 11:39:26 +0100

    Removed project

--- Phishing_Project/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LogInComponent } from './componente/log-in/log-in.component';
-import { LogInStemdoComponent } from './componente/log-in-stemdo/log-in-stemdo.component';
-
-export const routes: Routes = [
-    {path:"", component:LogInComponent},
-    {path:"stemdo", component:LogInStemdoComponent}
-];
