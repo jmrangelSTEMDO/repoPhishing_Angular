abc89eddd19702d8a2bd676e3f3f2631b2a42e85
jmrangelSTEMDO <jmrangel@stemdo.io>
2025-01-28 17:06:50 +0100

    delete all

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { config } from './app/app.config.client';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, config)
-  .catch((err) => console.error(err));
